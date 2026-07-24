e8647ecf224cab2d776df42f82cc7e765216d98a
Surya-Pratap12 <suryapratapsinghmahecha.06@gmail.com>
2026-07-24 19:47:00 +0530

    modify usermodel

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,7 +62,30 @@ userSchema.methods.isPasswordCorrect = async function
     return await brycpt.compare(password, this.password)
 }
 
-// userSchema.method.generateAccessToken = function()
-
+userSchema.method.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )    
+} 
 export const User = mongoose.model("User", userSchema)
 
